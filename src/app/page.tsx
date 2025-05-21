32a229e65576a688e304a4454ca2a72450d057ec
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2025-05-21 20:39:22 +1200

    Implement "number of people" input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-const numberOfPeople = 4;
+import { ChangeEvent, useState } from "react";
 
-const calculateRewards = () => {
+const NUMBER_OF_PEOPLE_DEFAULT = 4;
+const NUMBER_OF_PEOPLE_MIN = 1;
+const NUMBER_OF_PEOPLE_MAX = 20;
+const POINTS_DEFAULT = 0;
+const EPSILON = 0.001;
+
+const calculateRewards = (numberOfPeople: number) => {
   const pointsArray = [...Array(numberOfPeople).keys()].map((i) => {
     const points = document.getElementById(
       `points-${i.toString()}`,
     ) as HTMLInputElement;
-    return points.value ? Number(points.value) : 0;
+    return points.value ? Number(points.value) : POINTS_DEFAULT;
   });
   const avgPoints = pointsArray.reduce((a, b) => a + b, 0) / pointsArray.length;
   const deviations = pointsArray.map((points) => points - avgPoints);
@@ -21,46 +27,83 @@ const calculateRewards = () => {
   for (const points of pointsArray) {
     if (points > avgPoints) {
       rewards.push(
-        ((40 / (positiveDeviationsSum + 0.001)) * (points - avgPoints)).toFixed(
-          2,
-        ),
+        (
+          (40 / (positiveDeviationsSum + EPSILON)) *
+          (points - avgPoints)
+        ).toFixed(2),
       );
     } else if (points < avgPoints) {
       rewards.push(
         (
-          -(15 / (negativeDeviationsSumAbs + 0.001)) *
+          -(15 / (negativeDeviationsSumAbs + EPSILON)) *
           Math.abs(points - avgPoints)
         ).toFixed(2),
       );
     } else {
-      rewards.push((0).toFixed(2));
+      rewards.push(POINTS_DEFAULT.toFixed(2));
     }
   }
   console.log(rewards);
 };
 
 export default function Home() {
+  const [numberOfPeople, setNumberOfPeople] = useState(
+    NUMBER_OF_PEOPLE_DEFAULT,
+  );
+
+  const handleNumberOfPeopleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      const value = Number(e.target.value);
+      if (value >= NUMBER_OF_PEOPLE_MIN && value <= NUMBER_OF_PEOPLE_MAX) {
+        setNumberOfPeople(value);
+      } else if (value < NUMBER_OF_PEOPLE_MIN) {
+        setNumberOfPeople(NUMBER_OF_PEOPLE_MIN);
+      } else if (value > NUMBER_OF_PEOPLE_MAX) {
+        setNumberOfPeople(NUMBER_OF_PEOPLE_MAX);
+      }
+    }
+  };
+
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+        <button
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => {
+            calculateRewards(numberOfPeople);
+          }}
+        >
+          Calculate Points
+        </button>
+        <div className="flex">
+          <label className="w-40" htmlFor={"numberOfPeople"}>
+            Number of People:
+          </label>
+          <input
+            className="w-12 border-2 border-solid border-black text-center"
+            id={"numberOfPeople"}
+            type="number"
+            defaultValue={NUMBER_OF_PEOPLE_DEFAULT}
+            min={NUMBER_OF_PEOPLE_MIN}
+            max={20}
+            onChange={(e) => {
+              handleNumberOfPeopleChange(e);
+            }}
+          />
+        </div>
         {[...Array(numberOfPeople).keys()].map((i) => (
           <div className="flex" key={i}>
             <label className="w-24" htmlFor={`points-${i.toString()}`}>
               Points ({i + 1}):
             </label>
             <input
-              className="w-12 border-2 border-solid border-black"
+              className="w-12 border-2 border-solid border-black text-center"
               id={`points-${i.toString()}`}
               type="number"
+              defaultValue={POINTS_DEFAULT}
             />
           </div>
         ))}
-        <button
-          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
-          onClick={calculateRewards}
-        >
-          Calculate
-        </button>
       </main>
     </div>
   );
